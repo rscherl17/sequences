043ef4ace9189c6dead125970172611d9b555e6f
rscherl17 <robertscherl@wireless-s0-no-150-5-74.uchicago.edu>
2017-01-30 17:13:58 -0600

    1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get("/all_guesses", {:contoller => 'guess', :action => 'wild'})
+  get("/show_answer", {:contoller => 'guess', :action => 'show'})
 end
